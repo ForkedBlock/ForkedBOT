aac50c7048fb309ba9480211d9da9079166b431e
axelarator <ttp1597@gmail.com>
2018-11-04 01:20:51 -0400

    added npm packages

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./config.json");
+var request = require("request");
+
 bot.on("ready", () => {
   console.log("I'm a filthy bot, but I love to party! Let's Do this shit!");
 });
